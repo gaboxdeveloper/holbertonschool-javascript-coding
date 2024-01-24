c8eaca753ae1356c227390b394fc6ccf6092207d
Gabriel Rossy <grossy.sjb@gmail.com>
2024-01-24 23:21:03 +0000

    task3 semoistandard

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,12 +3,12 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 
-request (url, (err, response, body) => {
-  if (err){
+request(url, (err, response, body) => {
+  if (err) {
     console.log(err);
   } else {
-    const data = JSON.parse(body)
-    const title = data.title
+    const data = JSON.parse(body);
+    const title = data.title;
     console.log(title);
   }
 });
