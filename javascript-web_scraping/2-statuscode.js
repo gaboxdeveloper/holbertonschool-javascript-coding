014243b6f9540c2d25bb7264f6d5f6ebd5ed61e9
Gabriel Rossy <grossy.sjb@gmail.com>
2024-01-24 22:41:09 +0000

    task2 semistandard validation

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,26 +2,12 @@
 const request = require('request');
 
 const statusCode = (url) => {
-  request(url, (err, response) => {
+  request(url, (response) => {
     if (url) {
-      console.log('code:', response.statusCode)
+      console.log('code:', response.statusCode);
     }
   });
-}
+};
 
 const url = process.argv[2];
 statusCode(url);
-
-
-/* Write a script that display the status code of a GET request.
-
-The first argument is the URL to request (GET)
-The status code must be printed like this: code: <status code>
-You must use the module request
-
-guillaume@ubuntu:~/$ ./2-statuscode.js https://intranet.hbtn.io/status
-code: 200
-guillaume@ubuntu:~/$ ./2-statuscode.js https://intranet.hbtn.io/doesnt_exist
-code: 404
-guillaume@ubuntu:~/$ 
-*/
