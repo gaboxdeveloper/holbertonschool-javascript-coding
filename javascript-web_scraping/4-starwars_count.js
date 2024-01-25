6006088be8e61ee00174ba6c522664a1b1fc9721
Gabriel Rossy <grossy.sjb@gmail.com>
2024-01-25 00:07:07 +0000

    task4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const characterId = 18;
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const films = JSON.parse(body).results;
+    const filmsWithWedge = films.filter(film =>
+      film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)
+    );
+    console.log(filmsWithWedge.length);
+  }
+});
